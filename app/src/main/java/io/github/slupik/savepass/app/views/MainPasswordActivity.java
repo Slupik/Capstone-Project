47f297cb19fb80d351a575a25a98bcca41e69dee
Slupik <Slupik98@gmail.com>
2018-07-21 14:09:18 +0200

    feat: Create base for architecture in the future
    Because of lack of time architecture will be done after project approval
    by the Udacity team

---
+++ app/src/main/java/io/github/slupik/savepass/app/views/MainPasswordActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018. by Sebastian Witasik
+ * All rights reserved. No part of this application may be reproduced or be part of other software, without the prior written permission of the publisher. For permission requests, write to the author(WitasikSebastian@gmail.com).
+ */
+
+package io.github.slupik.savepass.app.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.slupik.savepass.R;
+
+public class MainPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_password);
+    }
+}
