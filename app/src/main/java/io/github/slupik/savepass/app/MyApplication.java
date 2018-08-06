e456ec4ade9f239372fa78458e1d5aa61503950c
Slupik <Slupik98@gmail.com>
2018-08-06 20:41:44 +0200

    feat: Implement Google Analytics

--- app/src/main/java/io/github/slupik/savepass/app/MyApplication.java
+++ app/src/main/java/io/github/slupik/savepass/app/MyApplication.java
@@ -7,10 +7,17 @@ package io.github.slupik.savepass.app;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+
+import io.github.slupik.savepass.R;
 import io.github.slupik.savepass.app.background.DispatcherController;
 
 public class MyApplication extends Application {
     private String appPassword = "";
+    private static GoogleAnalytics analytics;
+    private static Tracker tracker;
 
     public String getAppPassword() {
         return appPassword;
@@ -24,5 +31,28 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         DispatcherController.startJobs(getApplicationContext());
+        setupGoogleAnal();
+    }
+
+    private void setupGoogleAnal() {
+        analytics = GoogleAnalytics.getInstance(this);
+        tracker = analytics.newTracker(R.xml.global_tracker);
+
+        tracker.enableExceptionReporting(true);
+        // https://developers.google.com/analytics/devguides/collection/android/display-features
+        tracker.enableAdvertisingIdCollection(true);
+        tracker.enableAutoActivityTracking(true);
+
+        tracker.send(new HitBuilders.ScreenViewBuilder().setCustomDimension(1, null).build());
+    }
+
+    @SuppressWarnings("unused")
+    public static Tracker tracker() {
+        return tracker;
+    }
+
+    @SuppressWarnings("unused")
+    public static GoogleAnalytics analytics() {
+        return analytics;
     }
 }
